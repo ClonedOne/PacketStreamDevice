244b1e92b18138b2c14f7d140bf7fc7f6c9f3895
ClonedOne <severi.giorgio@gmail.com>
2017-03-01 14:51:45 +0100

    added header

---
+++ pktstream.h
@@ -0,0 +1,13 @@
+#ifndef PKTSTREAM_H
+#define PKTSTREAM_H
+
+
+#define DEVICE_NAME "pktstrm"
+#define MAJOR_NUM 75
+#define MAX_FILE_SIZE 10000000
+#define PKT_DEFAULT_SIZE 4096
+
+typedef unsigned char byte;
+
+
+#endif
